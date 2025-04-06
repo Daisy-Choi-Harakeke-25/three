c900a9774d4023cad2a594a843328786079440e9
Daisy Choi <chj15937@gmail.com>
2025-04-06 22:59:58 +1200

    added countCartItems

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,17 @@
 'use client'
+import useCart from '@/stores/cartStore'
 import { CldImage } from 'next-cloudinary'
 import Link from 'next/link'
+import { useShallow } from 'zustand/shallow'
 
 
 const Nav = () => {
+  const { cart } = useCart(
+    useShallow((state) => ({
+cart: state.cart,
+    }))
+  )
+  const countCartItems = cart.length
   return (
     <nav className="flex align-middle">
       <Link href="/" className="m-6">
@@ -27,7 +35,7 @@ const Nav = () => {
           <Link href="/contactUs">Contact us</Link>
         </p>
         <p>
-          <Link href="/cart">Cart</Link>
+          <Link href="/cart"  className='flex flex-row'>Cart<p className='bg-[#009B64] w-8 h-8 flex items-center justify-center rounded-full text-white ml-2'>{countCartItems}</p></Link>
         </p>
       </div>
     </nav>
