03902c9c7f9139e0a0ff155124d0f9b8436879c0
Daisy Choi <chj15937@gmail.com>
2025-04-09 19:09:26 +1200

    remove .env onfo from page.tsx

--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -1,4 +1,4 @@
-'use server'
+import { GetServerSideProps } from 'next'
 import MenuItemListComponent from '@/app/menu/[id]/MenuItemListComponent'
 import { MenuItemDraft } from '@/models/Model'
 import Link from 'next/link'
@@ -6,7 +6,7 @@ import Link from 'next/link'
 type PageProps = { params: Promise<{ id: string }> }
 
 async function getMenuItem(id: string) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/menu/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/menu/${id}`, {
     cache: 'no-store',
   })
   if (!res.ok) {
@@ -20,9 +20,21 @@ const Page = async ({ params }: PageProps) => {
   const menuItem: MenuItemDraft = await getMenuItem(id)
   if (!menuItem) return <p>Loading...</p>
   return (
-    <div className='mx-80'>
-      <p ><Link href='/' className='mx-2'>Home</Link>/<Link href='/menu' className='mx-2'>All products</Link>/<Link href={`${id}`} className='mx-2'>{menuItem.name}</Link></p>
-      <MenuItemListComponent menuItem={menuItem} id={id}/>
+    <div className="mx-80">
+      <p>
+        <Link href="/" className="mx-2">
+          Home
+        </Link>
+        /
+        <Link href="/menu" className="mx-2">
+          All products
+        </Link>
+        /
+        <Link href={`${id}`} className="mx-2">
+          {menuItem.name}
+        </Link>
+      </p>
+      <MenuItemListComponent menuItem={menuItem} id={id} />
     </div>
   )
 }
