4b3629391e49425899c3cf631423c5fe39427ef8
Daisy Choi <chj15937@gmail.com>
2025-04-09 23:31:48 +1200

    firebase hosting setup

---
+++ functions/index.ts
@@ -0,0 +1,13 @@
+import * as functions from 'firebase-functions';
+import * as next from 'next';
+import { Request, Response } from 'express';
+
+const dev = process.env.NODE_ENV !== 'production';  // To enable dev mode in local environment
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  exports.nextjs = functions.https.onRequest((req: Request, res: Response) => {
+    return handle(req, res);  // Handle all requests with Next.js
+  });
+});
